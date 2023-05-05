6a5df8263da573ca40047364e568f8bef7e5fee9
clipKoala <122833250+clipKoala@users.noreply.github.com>
2023-05-05 08:53:41 +0200

    possible solve, but still not ideal; -> need improvement

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'app_theme.dart';
+import 'screens/detail/detail_screen.dart';
+import 'screens/home/home.dart';
+import 'screens/mostpopular/most_popular_screen.dart';
+import 'screens/profile/profile_screen.dart';
+import 'screens/special_offers/special_offers_screen.dart';
+import 'screens/tabbar/tabbar.dart';
+import 'screens/test/test_screen.dart';
+
+
 void main() => runApp(const StartPage());
 
 class StartPage extends StatelessWidget {
@@ -16,3 +26,12 @@ class StartPage extends StatelessWidget {
     );
   }
 }
+
+final Map<String, WidgetBuilder> routes = {
+  HomeScreen.route(): (context) => const HomeScreen(title: '123'),
+  MostPopularScreen.route(): (context) => const MostPopularScreen(),
+  SpecialOfferScreen.route(): (context) => const SpecialOfferScreen(),
+  ProfileScreen.route(): (context) => const ProfileScreen(),
+  ShopDetailScreen.route(): (context) => const ShopDetailScreen(),
+  TestScreen.route(): (context) => const TestScreen(),
+};
