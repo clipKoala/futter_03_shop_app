60b5471019f87a7ca0584524747b4ec06d99bdbd
clipKoala <122833250+clipKoala@users.noreply.github.com>
2023-05-06 21:37:31 +0200

    tiny change

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class StartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Fresh-Buyer',
+      title: 'Buyer',
       theme: appTheme(),
       routes: routes,
       home: const FRTabbarScreen(),
